c025dc55dceb5519c38c714edf397e005e425019
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-24 22:11:44 -0400

    Created test read me

---
+++ REAMDMEtest.md
@@ -0,0 +1,48 @@
+# holdens project
+  
+  ##### Table of Contents  
+  [Description](#description)  
+  [URL](#url)  
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [License](#license)
+  [Tests](#tests)
+  [Questions](#quesetions)
+  
+  ## Description
+  this is my project
+
+ 
+  ## URL
+  <a name="url"/>
+  https://github.com/holdenmg/README-Generator
+  
+  
+  ## Installation
+  <a name="installation"/>
+  none
+  
+  ## Usage
+  <a name="usage"/>
+  Use this to generate readmes
+
+  ## Contributing
+  <a name="contributing"/>
+  none
+  
+  
+  
+  ## License
+  <a name="license"/>
+ 1
+ 
+ ## Tests
+ <a name="tests"/>
+ none
+ 
+ ## Questions
+ <a name="questions"/>
+ Please email holdenmgarrison@gmail.com for any questions
+ 
+ Github link: https://github.com/holdenmg
--- index.js
+++ index.js
@@ -2,39 +2,37 @@
 
 
 const inquirer = require('inquirer');
-  const fs = require('fs');
+const fs = require('fs');
   
-  const generateREADME = ({title, description, url, screenshotUrl, installation, usage, contributing, license, tests, email}) =>
+const generateREADME = ({title, description, url, installation, usage, contributing, license, tests, email, username, githubLink}) =>
   
   `# ${title}
   
+  ##### Table of Contents  
+  [Description](#description)  
+  [URL](#url)  
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [License](#license)
+  [Tests](#tests)
+  [Questions](#quesetions)
   
   ## Description
   ${description}
 
-  ##### Table of Contents  
-  [Headers](#headers)  
-  [Emphasis](#emphasis)  
-   
-  <a name="headers"/>
-  ## Headers
-
-
+ 
   ## URL
-  <a name="URL"/>
+  <a name="url"/>
   ${url}
-  
-  ## Screenshot
-  <a name="Screenshot"/>
-  <img src=${screenshotUrl}> 
   
   
   ## Installation
-  <a name="headers"/>
+  <a name="installation"/>
   ${installation}
   
   ## Usage
-  <a name="headers"/>
+  <a name="usage"/>
   ${usage}
 
   ## Contributing
@@ -53,17 +51,86 @@ const inquirer = require('inquirer');
  
  ## Questions
  <a name="questions"/>
- Please email ${email} for any questions`
+ Please email ${email} for any questions
+ 
+ Github link: https://github.com/${username}`
   
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Enter project description:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Enter URL for deployed project:',
+    name: 'url',
+  },
+  {
+    type: 'input',
+    message: 'Enter installation information:',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Enter usage information:',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Enter contributing information:',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Enter project description:',
+    name: 'description',
+  },
+  {
+    type: 'list',
+    message: 'Choose license:',
+    name: 'license',
+    choices: [1,2,3]
+  },
+  {
+    type: 'input',
+    message: 'Enter test information:',
+    name: 'tests',
+  },
+  {
+    type: 'input',
+    message: 'Enter your email:',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'Enter your github username:',
+    name: 'username',
+  },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const readmeContent = generateREADME(data);
+  fs.writeFile(fileName, readmeContent, (err) =>
+  err ? console.log(err) : console.log("success")
+  )};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+    writeToFile('REAMDMEtest.md', answers)
+    });
+
+}
 
 // Function call to initialize app
 init();
